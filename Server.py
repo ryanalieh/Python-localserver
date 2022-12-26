2c9a74367a2116e4582814bbed2382a2e31d1c46
RyanDevv <82663155+lxnar-dev@users.noreply.github.com>
2022-12-26 16:18:48 +0300

    Create Server.py

---
+++ Server.py
@@ -0,0 +1,27 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://github.com/lxnar-dev/</title></head>", "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<h1>First page</h1>", "utf-8"))
+        self.wfile.write(bytes("<p>localhost made with python by RyanDevv</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+        
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started at http://%s:%s" % (hostName, serverPort))
+    
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    
+    webServer.server_close()
+    print("Server stopped.")
